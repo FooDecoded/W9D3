ae3ed716aa76a4bd0ba0f4a186e75876c02763da
asem <foodecoded007@gmail.com>
2019-08-21 16:57:41 -0700

    Disks move, but stack out of order

--- Hanoi/src/hanoi-view.js
+++ Hanoi/src/hanoi-view.js
@@ -5,14 +5,22 @@ class HanoiView {
     this.setupTowers();
     // this.render();
     this.source = -1;
-    this.destination = -1;
     
     $(".towers").on("click", "ul" ,(e) => {
+      // debugger
       if (this.source === -1) {
-        this.source = e.target.data("idx")
-      } else if(this.destination === -1) {
-        this.destination = e.target.data("idx");
-        
+        this.source = $(e.currentTarget).data("idx");
+      } else {
+        debugger
+        let moved = this.game.isValidMove(this.source, $(e.currentTarget).data("idx"));
+        if (moved) {
+          this.game.move(this.source, $(e.currentTarget).data("idx"));
+          // debugger
+          // let el = this.piles[this.source].pop();
+          // this.piles[$(e.currentTarget).data("idx")].push(el);
+          this.render(this.source, $(e.currentTarget).data("idx"));
+        }
+        this.source = -1;
       }
     });
 
@@ -39,14 +47,18 @@ class HanoiView {
       towers[tower_idx].append($("<li>").data("idx", index).text(index));
     }
   });
-  towers.forEach((tower) => {
-    this.el.append(tower);
-  });
+    towers.forEach((tower) => {
+
+      this.el.append(tower);
+    });
+
   this.piles = towers;
 }
 
-  render() {
-
+  render(start, end) {
+    let firstPile = this.piles[start];
+    let el = firstPile.find(":first-child").remove(); 
+    this.piles[end].append(el);
   }
 }
 
